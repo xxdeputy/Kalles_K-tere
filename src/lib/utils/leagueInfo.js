ac53036b9b682053ddb5ddbc61306f3a74eb8096
Mathias <43956573+xxdeputy@users.noreply.github.com>
2023-11-29 20:23:35 +0100

    Update leagueInfo.js

--- src/lib/utils/leagueInfo.js
+++ src/lib/utils/leagueInfo.js
@@ -21,7 +21,7 @@ https://github.com/nmelhado/league-page/blob/master/TRAINING_WHEELS.md#ii-adding
 // To omit an optional field, set it's value to null
 
 export const managers = [
- {
+// {
  //  {
 // "managerID": "605738009211494400",  // the user's manager ID, go to https://api.sleeper.app/v1/league/<your_league_id>/users to find user IDs (you can use older leagueIDs to find user IDs for managers that are no longer in the league)
 // "name": "Mathias",
